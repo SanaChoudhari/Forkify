90b2a46e2de049604116bd7a2f99a02f07cd173c
SanaChoudhari <Sanac158@gmail.com>
2024-07-06 12:01:09 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8b168b02-e999-447e-9a76-2d176618c133';
+export const MODAL_CLOSE_SEC = 2.5;
